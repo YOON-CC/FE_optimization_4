3869d4e60b34c772128e919132eab6f972459023
YOON-CC <whdbscks77@gmail.com>
2023-09-10 16:15:51 +0900

    test

---
+++ after_optimization/src/containers/ImageModalContainer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import ImageModal from '../components/ImageModal';
+
+function ImageModalContainer() {
+  const modalVisible = useSelector(state => state.imageModal.modalVisible );
+  const bgColor = useSelector(state => state.imageModal.bgColor );
+  const src = useSelector(state => state.imageModal.src );
+  const alt = useSelector(state => state.imageModal.alt );
+
+
+
+  console.log("ImageModalContainer was rendered"); 
+  return (
+    <ImageModal
+      modalVisible={modalVisible}
+      bgColor={bgColor}
+      src={src}
+      alt={alt}
+    />
+  );
+}
+
+export default ImageModalContainer;
